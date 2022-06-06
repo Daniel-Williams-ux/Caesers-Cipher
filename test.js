a8f66eeb56b9b1648a863fc61972c291da235ea4
Daniel Williams <williamsoligodaniel@gmail.com>
2022-06-06 07:26:25 +0000

    test file

---
+++ test.js
@@ -0,0 +1,7 @@
+rot13("SERR PBQR PNZC") should decode to the string FREE CODE CAMP
+
+rot13("SERR CVMMN!") should decode to the string FREE PIZZA!
+
+rot13("SERR YBIR?") should decode to the string FREE LOVE?
+
+rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT.") should decode to the string THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.
