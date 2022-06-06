e1d44309820c1dad298b258b13e6d15538dafd11
Daniel Williams <williamsoligodaniel@gmail.com>
2022-06-06 07:22:53 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+function rot13(str) {
+  let solved = "";
+  for (let i = 0; i < str.length; i++) {
+    let asciNum = str[i].charCodeAt();
+    if (asciNum >= 65 && asciNum <= 77) {
+      solved += String.fromCharCode(asciNum + 13);
+    } else if (asciNum >= 78 && asciNum <= 90) {
+      solved +=  String.fromCharCode(asciNum - 13);
+    }else {
+      solved += str[i];
+    }
+    
+  }
+  return solved;
+}
+
+rot13("SERR PBQR PNZC");
